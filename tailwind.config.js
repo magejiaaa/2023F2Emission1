bbe528ef4fb9f7ca66a9e0773bfde4054495ebfc
magejiiii <dses60337@gmail.com>
2023-11-14 17:52:37 +0800

    刻選單、建置tailwind主題

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,41 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'sp-1': '4px',
+        'sp-2': '8px',
+        'sp-3': '16px',
+        'sp-4': '32px',
+        'sp-5': '48px',
+        'sp-6': '64px'
+      },
+      borderRadius: {
+        'sp-1': '4px',
+        'sp-2': '8px',
+        'sp-3': '16px',
+        'sp-4': '32px',
+        'sp-5': '48px',
+        'sp-6': '64px'
+      }
+    },
+    colors: {
+      'Grayscale': {
+        0: '#FFF',
+        1: '#E2E2E2',
+        2: '#C6C6C6',
+        3: '#8C8C8C',
+        4: '#444',
+        5: '#000'
+      },
+      'Primary': {
+        1: '#FBFFE1',
+        2: '#EEFFA9',
+        3: '#D1FA31',
+        4: '#BBE217',
+        5: '#A8C400'
+      }
+    }
   },
   plugins: [],
 }

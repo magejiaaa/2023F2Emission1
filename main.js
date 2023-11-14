b7fd1e214c3e454a562c5babceba76a8720f6258
magejiaaa <dses60337@gmail.com>
2023-11-14 22:36:15 +0800

    新增GSAP、banner與候選主張

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import './src/index.css'
+
+function createBannerAnimation(elementId) {
+    gsap.to(elementId, {
+        x: '-100%',
+        duration: 15,
+        repeat: -1,
+        ease: "linear",
+        onComplete: function () {
+            // 在動畫完成後設置回調函數
+            gsap.set(elementId, { x: '200%' });
+        }
+    });
+}
+
+createBannerAnimation("#firstBannerText");
+createBannerAnimation("#secondBannerText");
+createBannerAnimation("#thirdBannerText");
